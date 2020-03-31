c9cb23f43b5c89c231d960ef40527c120dda31b0
bpt2017 <32851863+bpt2017@users.noreply.github.com>
2020-03-31 16:54:54 -0400

    Fixed not importing db when running the app.

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -24,5 +23,5 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    
+    db.init_app(app)
     app.run(port=5000, debug=True)
